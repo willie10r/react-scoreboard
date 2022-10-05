1df9fc6cfe41b18d700af9547afba4e53ecb9cee
willie m <zx10rwillie@gmail.com>
2022-10-05 18:27:16 -0500

    first react app useing 'create react app'

---
+++ src/components/App.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Header from './Header';
+import Player from './Player';
+import Counter from './Counter';
+
+
+
+const App = () => {
+  const [players, setPlayers] = React.useState([
+    {
+      name: "Guil",
+      id: 1
+    },
+    {
+      name: "Treasure",
+      id: 2
+    },
+    {
+      name: "Ashley",
+      id: 3
+    },
+    {
+      name: "James",
+      id: 4
+    }
+  ]);
+
+  const handleRemovePlayer = (id) => {
+    setPlayers(prevPlayers => prevPlayers.filter(p => p.id !== id));
+  }
+
+  return (
+    <div className="scoreboard">
+      <Header
+        title="Scoreboard"
+        totalPlayers={players.length}
+      />
+
+      {/* Players list */}
+      {players.map(player =>
+        <Player
+          name={player.name}
+          id={player.id}
+          key={player.id.toString()}
+          removePlayer={handleRemovePlayer}
+        />
+      )}
+    </div>
+  );
+
+}
+
+export default App;
